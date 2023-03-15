b8a1df8fed92928df1c9ce2208995b2c4900d7b0
Akash Mudgal <akashmudgal66403@gmail.com>
2023-03-16 00:27:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ListScreen />} />
+        <Route path="/test-app" element={<ListScreen />} />
         <Route path="/showdetails/:id" element={<ShowDetails />} />
       </Routes>
     </Router>
